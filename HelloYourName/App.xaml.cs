1704e7887f58bbee2c6d791c87031014263428dd
Grace Tay <gracektay@Graces-Mac-mini.local>
2016-11-12 21:50:56 -0500

    Initial Commit

---
+++ HelloYourName/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿/*
+Grace Tay
+IT-230 Software Development with C#.NET
+10/28/2016
+1-3 Coding Activity: Your First C# WPF Application (HelloYourName)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloYourName
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ HelloYourName/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿/*
+Grace Tay
+IT-230 Software Development with C#.NET
+10/28/2016
+1-3 Coding Activity: Your First C# WPF Application (HelloYourName)
+
+Summary: Added okClick function which shows a message to the user reflecting the name the user typed in the textbox.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloYourName
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello " + userName.Text);
+        }
+    }
+}
